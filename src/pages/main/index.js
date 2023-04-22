0e9578ef1e39626ca64239ed36ac00d3410a6a1f
이나현 <siennahewer@gmail.com>
2023-04-22 12:24:33 +0900

    fear: 메인 페이지 배너 슬라이드 기능 추가

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,45 +1,13 @@
-document.querySelector("#productContainer").innerHTML = `<article class="product">
-  <div>
-    <img src="" alt="">
-  </div>
-  <h3 class="product-name"></h3>
-  <p class="product-price"></p>
-</article>`;
+import MainBanner from "../../components/banner/bannerSlide.js";
+import Layout from "../../components/layout/index.js";
+import ProdcutCard from "../../components/product/product.js";
 
-document.querySelectorAll(".product").forEach((product) => {
-  product.style.cssText = `
-  display: inline-block;
-	margin: 10px 10px;`;
-});
-
-document.querySelectorAll(".product div").forEach((div) => {
-  div.style.cssText = `
-  overflow: hidden;
-	height: 330px;
-	width: 251px;`;
-});
-
-document.querySelectorAll(".product img").forEach((img) => {
-  img.addEventListener("mouseout", () => {
-    img.style.cssText = `
-    object-fit: cover;
-    height: 100%;
-    transition: transform 1s;`;
+const run = () => {
+  window.addEventListener("DOMContentLoaded", async () => {
+    Layout();
+    MainBanner();
+    ProdcutCard();
   });
-  img.addEventListener("mouseover", () => {
-    img.style.cssText = `
-    transform: scale(1.1);
-    transition: transform 1s;`;
-  });
-});
+};
 
-document.querySelectorAll(".product-name").forEach((name) => {
-  name.style.cssText = `
-  font-size: 18px;
-	margin: 20px 0;`;
-});
-
-document.querySelectorAll(".product-price").forEach((price) => {
-  price.style.cssText = `
-  font-size: 18px;`;
-});
+run();
