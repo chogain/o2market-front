447e4028b1a686d99ff56ea0b0050d687d8e7be8
Gain Cho <chogain05@gmail.com>
2023-04-24 17:43:57 +0900

    src/components/layout 내부 topbutton.js 추가 index.js와 layout.css 변경, global.css에 연두색 추가

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,9 @@
 import Footer from "./footer.js";
 import Header from "./header.js";
+import topButton from "./topbutton.js";
 
 export default function Layout() {
   Header();
   Footer();
+  topButton();
 }
