1ef5acf9f62a65fccf7558a49ab24051bcc80471
Gain Cho <chogain05@gmail.com>
2023-04-25 17:08:03 +0900

    장바구니 전체선택 버튼으로 모든 체크박스 토글, 선택 상품 삭제 기능 구현

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -4,86 +4,88 @@ document.addEventListener("DOMContentLoaded", function () {
 
   for (let i = 0; i < localStorage.length; i++) {
     const cartItem = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    itemContainer.insertAdjacentHTML(
-      "beforeend",
-      `<article class="add-cart">
-                <input type="checkbox" class="checking"/>
-                <span class="product">${cartItem.productName}</span>
-                <span class="price">${cartItem.price}</span>
-                <div class="count-box">
-                  <div class="minus">-</div>
-                  <p class="count">${cartItem.count}</p>
-                  <div class="plus">+</div>
-                </div>
-                <span class="total-price">${addComma(
-                  cartItem.price * cartItem.count,
-                )}원</span>
-              </article>`,
-    );
-    resultPrice += cartItem.price * cartItem.count;
+    const totalPrice = cartItem.price * cartItem.count;
+    const itemHtml = `
+      <article class="add-cart">
+        <input type="checkbox" class="checking" checked />
+        <span class="product">${cartItem.productName}</span>
+        <span class="price">${cartItem.price}</span>
+        <div class="count-box">
+          <div class="minus">-</div>
+          <p class="count">${cartItem.count}</p>
+          <div class="plus">+</div>
+        </div>
+        <span class="total-price">${addComma(totalPrice)}원</span>
+      </article>`;
+    itemContainer.insertAdjacentHTML("beforeend", itemHtml);
+    resultPrice += totalPrice;
   }
-  document.querySelector("#sum-all-items").innerHTML = `${addComma(resultPrice)}원`;
-  document.querySelector("#total-price").innerHTML = `${addComma(resultPrice + 3000)}원`;
 
-  // minus 버튼에 이벤트 리스너 등록
-  document.querySelectorAll(".minus").forEach((minus) => {
-    const next = minus.nextElementSibling;
-    minus.addEventListener("click", () => {
-      if (parseInt(next.innerHTML) <= 1) {
-        alert("최소 구매 수량은 1개 입니다.");
+  const sumAllItems = document.querySelector("#sum-all-items");
+  const totalprice = document.querySelector("#total-price");
+  sumAllItems.innerHTML = `${addComma(resultPrice)}원`;
+  totalprice.innerHTML = `${addComma(resultPrice + 3000)}원`;
+
+  itemContainer.addEventListener("click", (event) => {
+    if (event.target.matches(".minus, .plus")) {
+      const countElem = event.target.parentNode.querySelector(".count");
+      const priceElem = event.target.parentNode.previousElementSibling;
+      const totalPriceElem = event.target.parentNode.nextElementSibling;
+      const price = parseInt(priceElem.innerHTML);
+      let count = parseInt(countElem.innerHTML);
+      if (event.target.classList.contains("plus")) {
+        count += 1;
+      } else if (count > 1) {
+        count -= 1;
       } else {
-        next.innerHTML = parseInt(next.innerHTML) - 1;
-        /* 해당 상품의 합계 바꿈 */
-        const thisPrice = parseInt(
-          next.parentElement.previousElementSibling.innerHTML.replace(/[^\d]/g, ""),
-        );
-        const thisPriceSum = parseInt(
-          next.parentElement.nextElementSibling.innerHTML.replace(/[^\d]/g, ""),
-        );
-        next.parentElement.nextElementSibling.innerHTML = `${addComma(
-          thisPriceSum - thisPrice,
-        )}원`;
-        /* 상품금액, 결재예정금액 바꿈 */
-        const price = parseInt(
-          next.parentElement.previousElementSibling.innerHTML.replace(/[^\d]/g, ""),
-        );
-        const sumAllItems = parseInt(
-          document.querySelector("#sum-all-items").innerHTML.replace(/[^\d]/g, ""),
-        );
-        document.querySelector("#sum-all-items").innerHTML = `${addComma(
-          sumAllItems - price,
-        )}원`;
+        alert("최소 구매 수량은 1개 입니다.");
       }
-    });
+      countElem.innerHTML = count;
+      const totalPrice = price * count;
+      totalPriceElem.innerHTML = `${addComma(totalPrice)}원`;
+      const itemsPrice = Array.from(
+        document.querySelectorAll(".add-cart .total-price"),
+      ).reduce((acc, item) => acc + parseInt(item.textContent.replace(/[^\d]/g, "")), 0);
+      sumAllItems.innerHTML = `${addComma(itemsPrice)}원`;
+      totalprice.innerHTML = `${addComma(itemsPrice + 3000)}원`;
+    }
   });
 
-  // plus 버튼에 이벤트 리스너 등록
-  document.querySelectorAll(".plus").forEach((plus) => {
-    const pre = plus.previousElementSibling;
-    plus.addEventListener("click", () => {
-      pre.innerHTML = parseInt(pre.innerHTML) + 1;
-      /* 해당 상품의 합계 바꿈 */
-      const thisPrice = parseInt(
-        pre.parentElement.previousElementSibling.innerHTML.replace(/[^\d]/g, ""),
-      );
-      const thisPriceSum = parseInt(
-        pre.parentElement.nextElementSibling.innerHTML.replace(/[^\d]/g, ""),
-      );
-      pre.parentElement.nextElementSibling.innerHTML = `${addComma(
-        thisPriceSum + thisPrice,
-      )}원`;
-      /* 상품금액, 결재예정금액 바꿈 */
-      const price = parseInt(
-        pre.parentElement.previousElementSibling.innerHTML.replace(/[^\d]/g, ""),
-      );
+  const toggleAll = document.querySelector(".all-select-text");
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-      const sumAllItems = parseInt(
-        document.querySelector("#sum-all-items").innerHTML.replace(/[^\d]/g, ""),
-      );
-      document.querySelector("#sum-all-items").innerHTML = `${addComma(
-        sumAllItems + price,
-      )}원`;
-    });
+  toggleAll.addEventListener("click", () => {
+    if (toggleAll.innerHTML == "전체선택해제") {
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = !checkbox.checked;
+      });
+    } else {
+      checkboxes.forEach((checkbox) => {
+        checkbox.checked = checkbox.checked;
+      });
+      toggleAll.innerHTML == "전체선택";
+    }
+  });
+  const deletebtn = document.querySelector("#partialDelete");
+  deletebtn.addEventListener("click", () => {
+    if (
+      document.querySelectorAll('input[type="checkbox"].checking:checked').length === 0
+    ) {
+      alert("삭제할 상품이 없습니다.");
+    }
+    document
+      .querySelectorAll('input[type="checkbox"].checking:checked')
+      .forEach((checkbox) => checkbox.closest(".add-cart").remove());
+    const itemsPrice = Array.from(
+      document.querySelectorAll(".add-cart .total-price"),
+    ).reduce((acc, item) => acc + parseInt(item.textContent.replace(/[^\d]/g, "")), 0);
+    sumAllItems.innerHTML = `${addComma(itemsPrice)}원`;
+    totalprice.innerHTML = `${addComma(itemsPrice + 3000)}원`;
+    if (itemsPrice == 0) {
+      if (!confirm("장바구니의 모든 상품이 삭제됩니다.")) {
+        location.reload();
+      }
+    }
   });
 });
 
