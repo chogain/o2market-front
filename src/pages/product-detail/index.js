919084315df94f68ca083cd9240dbdfccb0db3a7
Gain Cho <chogain05@gmail.com>
2023-04-25 09:12:00 +0900

    장바구니, 바로 구매 누를 경우 local storage에 json형태로 데이터 저장

--- src/pages/product-detail/index.js
+++ src/pages/product-detail/index.js
@@ -9,10 +9,10 @@ const plusBtns = document.querySelectorAll(".plus");
 const countEls = document.querySelectorAll(".count");
 const totalPriceEls = document.querySelectorAll(".total-price");
 
-// 구매 수량
+/* 구매 수량 */
 let count = parseInt(countEls[0].textContent);
 
-// 로컬에 있는 json 데이터 불러오기
+/* 로컬에 있는 json 데이터 불러오기 */
 fetch("product.json")
   .then((res) => res.json())
   .then((datas) => {
@@ -70,39 +70,82 @@ fetch("product.json")
         // });
       });
     });
+
+    /* 장바구니 정보 local storage에 등록 */
+    const sendLocalStorageBtn = document.querySelectorAll(".storage-btn");
+    sendLocalStorageBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        let maxIndex = 0;
+        let isSameProduct = false;
+        let sameProductIndex = -1;
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          const data = JSON.parse(localStorage.getItem(key));
+          if (data.productName === productInfo.productName) {
+            isSameProduct = true;
+            sameProductIndex = key;
+            break;
+          }
+          maxIndex = Math.max(maxIndex, Number(key));
+        }
+
+        /* local storage에 같은 상품이 있을 경우 count만 추가 */
+        let dataToSave;
+        if (isSameProduct) {
+          dataToSave = {
+            id: sameProductIndex,
+            productName: productInfo.productName,
+            price: productInfo.price,
+            count: count + JSON.parse(localStorage.getItem(sameProductIndex)).count,
+          };
+        } else {
+          const newId = maxIndex + 1;
+          dataToSave = {
+            id: newId,
+            productName: productInfo.productName,
+            price: productInfo.price,
+            count,
+          };
+        }
+
+        localStorage.setItem(dataToSave.id, JSON.stringify(dataToSave));
+      });
+    });
   });
 
-// /* 해당 상품의 상품평 등록 */
-// fetch("user.json")
-//   .then((res) => res.json())
-//   .then((users) => {
-//     const reviews = [];
-//     for (const item of users) {
-//       for (const review of item.reviews) {
-//         if (review.productId === "4") {
-//           const masckedName = item.name.charAt(0) + "*".repeat(item.name.length - 1);
-//           const newDiv = document.createElement("div");
-//           newDiv.innerHTML = `
-//             <div class="bottom-text">${masckedName}님</div>
-//             <p class="review-text">${review.review}</p>`;
-//           reviews.push(newDiv);
-//         }
-//       }
-//     }
-//     if (reviews.length === 0) {
-//       const noReviewText = document.createTextNode("리뷰가 없습니다.");
-//       reviewEl.appendChild(noReviewText);
-//     } else {
-//       const countReview = document.createElement("div");
-//       countReview.innerHTML = `총 <span class="font">${reviews.length}</span>개의 리뷰가 있습니다.`;
-//       document.querySelector(".review-counter").innerText = reviews.length;
-//       countReview.classList.add("review-count", "bottom-text");
-//       reviewEl.appendChild(countReview);
-//       for (const review of reviews) {
-//         reviewEl.appendChild(review);
-//       }
-//     }
-//   });
+/* 해당 상품의 상품평 등록 */
+fetch("user.json")
+  .then((res) => res.json())
+  .then((users) => {
+    const reviews = [];
+    if (!users.review) {
+      const noReviewText = document.createElement("p");
+      noReviewText.innerText = "리뷰가 없습니다.";
+      noReviewText.classList.add("bottom-text");
+      reviewEl.appendChild(noReviewText);
+    } else {
+      for (const item of users) {
+        for (const review of item.reviews) {
+          if (review.productId === "4") {
+            const masckedName = item.name.charAt(0) + "*".repeat(item.name.length - 1);
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = `
+            <div class="bottom-text">${masckedName}님</div>
+            <p class="review-text">${review.review}</p>`;
+            reviews.push(newDiv);
+          }
+        }
+      }
+      const countReview = document.createElement("div");
+      countReview.innerHTML = `총 <span class="font">${reviews.length}</span>개의 리뷰가 있습니다.`;
+      document.querySelector(".review-counter").innerText = reviews.length;
+      countReview.classList.add("review-count", "bottom-text");
+      reviewEl.appendChild(countReview);
+      for (const review of reviews) {
+        reviewEl.appendChild(review);
+      }
+    }
+  });
 
 /* 같은 클래스를 가진 태그에 같은 값 삽입 */
 function PutSameValueinSameClass(elements, inputValue) {
@@ -111,21 +154,121 @@ function PutSameValueinSameClass(elements, inputValue) {
   });
 }
 
+// // 두번째 시도
+// window.location.href = "http://localhost:5500/api/v1/products/64410645ad088180b3542f78";
+// fetch("http://localhost:5500/api/v1/products/64410645ad088180b3542f78")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     const productName = data.productName;
+//     const imgUrl = data.imageUrl;
+//     const description = data.description;
+//     const price = data.price;
+
+//     /* 받아온 데이터 삽입 */
+//     if (!imgUrl) {
+//       PutSameValueinSameClass(imgEls, "이미지 로드 실패");
+//     }
+//     PutSameValueinSameClass(imgEls, `<img src="${imgUrl}" alt="${description}">`);
+//     PutSameValueinSameClass(productNameEls, productName);
+//     PutSameValueinSameClass(priceEls, addComma(price));
+//     for (let i = 0; i < totalPriceEls.length; i++) {
+//       totalPriceEls[i].textContent = `${addComma(parseInt(price) * count)}원`;
+//     }
+//     companyEl.innerHTML = data.company;
+
+//     /* 구매수량 버튼 활성화 */
+//     minusBtns.forEach((minusBtn) => {
+//       minusBtn.addEventListener("click", () => {
+//         if (count <= 1) {
+//           alert("최소 구매 수량은 1개 입니다.");
+//           return;
+//         }
+
+//         count--;
+//         countEls.forEach((countEl) => {
+//           countEl.textContent = count;
+//         });
+//         totalPriceEls.forEach((totalPriceEl) => {
+//           totalPriceEl.textContent = `${addComma(parseInt(price) * count)}원`;
+//         });
+//       });
+//     });
+
+//     plusBtns.forEach((plusBtn) => {
+//       plusBtn.addEventListener("click", () => {
+//         count++;
+//         countEls.forEach((countEl) => {
+//           countEl.textContent = count;
+//         });
+//         totalPriceEls.forEach((totalPriceEl) => {
+//           totalPriceEl.textContent = `${addComma(parseInt(price) * count)}원`;
+//         });
+//       });
+//     });
+//   });
+
+// // 서버랑 연결되는지 확인해야하는 코드
+// window.location.href = "http://localhost:5500/api/v1/products/64410645ad088180b3542f78";
+// const getProductData = async () => {
+//   try {
+//     const response = await fetch(
+//       "http://localhost:5500/api/v1/products/64410645ad088180b3542f78",
+//     );
+//     const response = await fetch("./product.json");
+//     const productData = await response.json();
+//     console.log(productData[0]);
+//     const productName = productData[0].productName;
+//     const imgUrl = productData[0].imageUrl;
+//     const description = productData[0].description;
+//     const price = productData[0].price;
+//     const _id = productData[0]._id;
+
+//     /* 받아온 데이터 삽입 */
+//     if (!imgUrl) {
+//       PutSameValueinSameClass(imgEls, "이미지 로드 실패");
+//     }
+//     PutSameValueinSameClass(imgEls, `<img src="${imgUrl}" alt="${description}">`);
+//     PutSameValueinSameClass(productNameEls, productName);
+//     PutSameValueinSameClass(priceEls, addComma(price));
+//     for (let i = 0; i < totalPriceEls.length; i++) {
+//       totalPriceEls[i].textContent = `${addComma(parseInt(price) * count)}원`;
+//     }
+//     companyEl.innerHTML = productData.company;
+
+//     /* 구매수량 버튼 활성화 */
+//     minusBtns.forEach((minusBtn) => {
+//       minusBtn.addEventListener("click", () => {
+//         if (count <= 1) {
+//           alert("최소 구매 수량은 1개 입니다.");
+//           return;
+//         }
+
+//         count--;
+//         countEls.forEach((countEl) => {
+//           countEl.textContent = count;
+//         });
+//         totalPriceEls.forEach((totalPriceEl) => {
+//           totalPriceEl.textContent = `${addComma(parseInt(price) * count)}원`;
+//         });
+//       });
+//     });
+
+//     plusBtns.forEach((plusBtn) => {
+//       plusBtn.addEventListener("click", () => {
+//         count++;
+//         countEls.forEach((countEl) => {
+//           countEl.textContent = count;
+//         });
+//         totalPriceEls.forEach((totalPriceEl) => {
+//           totalPriceEl.textContent = `${addComma(parseInt(price) * count)}원`;
+//         });
+//       });
+//     })
+//   }
+// };
+
 /* 가격에 ,(쉼표) 삽입 */
 function addComma(price) {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
-// 라우터...?
-// const { Router } = require('express');
-//  const router.get("/products/:productId", async (req, res, next) => {
-
-//   try {
-//     const { productId } = req.params;
-//   const product = await totalData.findOne({ productId });
-
-//   } catch (err) {
-//     console.log(err.message);
-//     alert("에러가 발생했습니다.")
-//   }
-//  })
